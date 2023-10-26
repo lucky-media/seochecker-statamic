775d2e63348850d2c5140624ebd754ebb1cbf2aa
isni-sh <isnishabani.95@gmail.com>
2023-10-26 13:44:23 +0200

    create Linkedin card

---
+++ resources/js/components/meta/LinkedinCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3 class="mb-2">Linkedin</h3>
+        <a :href="url" target="_blanket" class="inline-block sp-w-[500px] shadow">
+            <div class="sp-h-[274px] sp-relative sp-bg-white sp-font-sm">
+                <div v-if="image" :style="{ backgroundImage: `url(${image})` }" class="sp-w-full sp-h-full"></div>
+                <img v-if="!image" src="@/assets/no-image.png"
+                    class="sp-h-full sp-bg-gray-50 sp-w-full sp-object-contain" />
+            </div>
+            <div class="p-3 bg-white">
+                <h4 class="mb-2 text-base">{{ site_name }}</h4>
+                <p class="sp-truncate sp-whitespace-nowrap sp-text-xs sp-uppercase sp-leading-[11px] sp-text-gray-600">
+                    {{ url }}
+                </p>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        site_name: String,
+        image: String,
+        url: String,
+    },
+};
+</script>
--- resources/js/components/reports/sections/MetaPreview.vue
+++ resources/js/components/reports/sections/MetaPreview.vue
@@ -1,37 +1,36 @@
 <template>
-    <div class="sp-space-y-4 sp-pt-4">
-        
-        <!-- Facebook -->
-        <facebook-card 
-            :image="og_graph.image"
-            :description="og_graph.description"
-            :site_name="og_graph.site_name"
-            :url="og_graph.url"
-        />
+  <div class="sp-space-y-4 sp-pt-4">
+    <!-- Facebook -->
+    <facebook-card
+      :image="og_graph.image"
+      :description="og_graph.description"
+      :site_name="og_graph.site_name"
+      :url="og_graph.url"
+    />
 
-         <!-- Twitter -->
-        <twitter-card 
-            :image="twitter.image"
-            :url="twitter.url"
-        />
+    <!-- Twitter -->
+    <twitter-card :image="twitter.image" :url="twitter.url" />
 
-    </div>
-
+    <!-- Linkedin -->
+    <linkedin-card
+      :site_name="og_graph.site_name"
+      :image="og_graph.image"
+      :url="og_graph.url"
+    />
+  </div>
 </template>
 
 <script>
 import FacebookCard from '../../meta/FacebookCard.vue';
 import TwitterCard from '../../meta/TwitterCard.vue';
+import LinkedinCard from '../../meta/LinkedinCard.vue';
 
 export default {
-    components: {
-        FacebookCard,
-        TwitterCard
-    },
-    props: ['og_graph', 'twitter'],
-    mounted() {
-      console.log(this.og_graph)  
-    },
-    components: {  FacebookCard, TwitterCard }
-}
+  components: {
+    FacebookCard,
+    TwitterCard,
+    LinkedinCard,
+  },
+  props: ['og_graph', 'twitter'],
+};
 </script>
