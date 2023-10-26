cf21e4a42d4f6950e91e5e66fc92014e77eb74e1
isni-sh <isnishabani.95@gmail.com>
2023-10-26 14:12:07 +0200

    update component props

--- resources/js/components/meta/TwitterCard.vue
+++ resources/js/components/meta/TwitterCard.vue
@@ -1,29 +1,37 @@
 <template>
-    <div>
-        <a :href="url" target="_blanket" class="inline-block sp-relative sp-overflow-hidden sp-rounded-md">
-            <div class="sp-w-[438px] sp-h-[260px] sp-relative sp-bg-white sp-font-sm">
-                <div v-if="card === 'summary_large_image'" :style="{ backgroundImage: `url(${image})` }"
-                    class="sp-w-full sp-h-full"></div>
-                <img v-if="card !== 'summary_large_image'" src="@/assets/no-image.png"
-                    class="sp-bg-gray-300 sp-w-full sp-object-contain sp-h-full" />
-                <p
-                    class="px-3 py-1 text-xs text-gray-800 sp-rounded-md sp-shadow-sm sp-absolute sp-left-2 sp-bottom-2 sp-bg-white/80">
-                    {{ url || 'www.google.com' }}
-                </p>
-            </div>
-        </a>
-    </div>
+  <div>
+    <a
+      :href="url"
+      target="_blanket"
+      class="inline-block sp-relative sp-overflow-hidden sp-rounded-md"
+    >
+      <div class="sp-w-[438px] sp-h-[260px] sp-relative sp-bg-white sp-font-sm">
+        <div
+          v-if="card === 'summary_large_image'"
+          :style="{ backgroundImage: `url(${image})` }"
+          class="sp-w-full sp-h-full"
+        ></div>
+        <img
+          v-if="card !== 'summary_large_image'"
+          src="@/assets/no-image.png"
+          class="sp-bg-gray-300 sp-w-full sp-object-contain sp-h-full"
+        />
+        <p
+          class="px-3 py-1 text-xs text-gray-800 sp-rounded-md sp-shadow-sm sp-absolute sp-left-2 sp-bottom-2 sp-bg-white/80"
+        >
+          {{ url || 'www.google.com' }}
+        </p>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        title: String,
-        url: String,
-        image: String,
-        site_name: String,
-        description: String,
-        card: String
-    },
-}
+  props: {
+    url: String,
+    image: String,
+    card: String,
+  },
+};
 </script>
--- resources/js/components/reports/sections/MetaPreview.vue
+++ resources/js/components/reports/sections/MetaPreview.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="sp-space-y-4 sp-pt-4">
     <!-- Facebook -->
-    <facebook-card :image="og_graph.image" :description="og_graph.description" :site_name="og_graph.site_name"
-      :url="og_graph.url" />
+    <facebook-card
+      :image="og_graph.image"
+      :desc="og_graph.description"
+      :site_name="og_graph.site_name"
+      :url="og_graph.url"
+    />
 
     <!-- Twitter -->
-    <twitter-card :image="twitter.image" :url="twitter.url" />
+    <twitter-card
+      :image="twitter.image"
+      :url="twitter.url"
+      :card="twitter.card"
+    />
 
     <!-- Linkedin -->
-    <linkedin-card :site_name="og_graph.site_name" :image="og_graph.image" :url="og_graph.url" />
+    <linkedin-card
+      :site_name="og_graph.site_name"
+      :image="og_graph.image"
+      :url="og_graph.url"
+    />
 
     <!-- Discord -->
-    <discord-card :site_name="og_graph.site_name" :image="og_graph.image" :url="og_graph.url"
-      :desc="og_graph.description" />
-
+    <discord-card
+      :site_name="og_graph.site_name"
+      :image="og_graph.image"
+      :url="og_graph.url"
+      :desc="og_graph.description"
+    />
   </div>
 </template>
 
