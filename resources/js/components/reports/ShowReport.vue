9ab89abf52d6dd3260b19a0a52522694a81b140a
isni-sh <isnishabani.95@gmail.com>
2023-10-26 16:14:48 +0200

    Display dynamic open graph tags

--- resources/js/components/reports/ShowReport.vue
+++ resources/js/components/reports/ShowReport.vue
@@ -1,26 +1,28 @@
 <template>
-<div>
+  <div>
     <performance-section :scores="report.lighthouse" class="mb-4" />
     <audit-section :audit="report.lighthouse.audit" />
     <seo-section :seo="report.seo" />
+    <meta-section :audit="report.og_graph.audit" />
     <meta-preview :og_graph="report.og_graph" :twitter="report.twitter" />
-</div>
+  </div>
 </template>
 
 <script>
-import SeoSection from "./sections/SeoSection.vue";
-import PerformanceSection from "./sections/PerformanceSection.vue";
-import AuditSection from "./sections/AuditSection.vue";
-import MetaPreview from "./sections/MetaPreview.vue";
+import SeoSection from './sections/SeoSection.vue';
+import PerformanceSection from './sections/PerformanceSection.vue';
+import AuditSection from './sections/AuditSection.vue';
+import MetaPreview from './sections/MetaPreview.vue';
+import MetaSection from './sections/MetaSection.vue';
 
 export default {
-    components: {
-        AuditSection,
-        PerformanceSection,
-        SeoSection,
-        MetaPreview,
-    },
-    props: ['report'],
-
-} 
+  components: {
+    AuditSection,
+    PerformanceSection,
+    SeoSection,
+    MetaPreview,
+    MetaSection,
+  },
+  props: ['report'],
+};
 </script>
---
+++ resources/js/components/reports/sections/MetaSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <card class="mt-4">
+    <card-header> Open Graph Tags </card-header>
+
+    <div v-for="(value, key) in audit" :key="key">
+      <info-item :title="unslugify(key)" :score="value.score">
+        <div class="flex flex-col items-start space-y-2">
+          <p v-if="key.score === 'failed'">{{ value.description }}</p>
+          <p>
+            <b>{{ value.value }}</b>
+          </p>
+        </div>
+      </info-item>
+    </div>
+  </card>
+</template>
+
+<script>
+import Card from '../../card/Card.vue';
+import CardHeader from '../../card/CardHeader.vue';
+import InfoItem from '../../infolist/InfoItem.vue';
+
+export default {
+  components: { Card, CardHeader, InfoItem },
+  props: ['audit'],
+  methods: {
+    unslugify(title) {
+      return title
+        .replace(/\_/g, ' ')
+        .replace(
+          /\w\S*/g,
+          (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+        );
+    },
+  },
+};
+</script>
