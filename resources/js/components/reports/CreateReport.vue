8d5c69663de3d892a545565b0da149353bf18b68
isni-sh <isnishabani.95@gmail.com>
2023-10-30 16:36:41 +0100

    Small fix

--- resources/js/components/reports/CreateReport.vue
+++ resources/js/components/reports/CreateReport.vue
@@ -1,85 +1,90 @@
 <template>
-    <div class="max-w-xl">
-        <div class="card">
-            <div class="flex flex-row space-x-4">
-                <text-input class="w-full" placeholder="Domain" v-model="domain" />
-                <button class="btn-primary" :disabled="disabled" @click="createReport">
-                    <div v-if="loading" class="flex items-center w-5 h-5 mr-2">
-                        <loading-graphic inline text="" />
-                    </div>
-                    Create Report
-                </button>
-            </div>
-            <p v-if="error" v-text="error" class="my-2 text-red-500 text-sm"></p>
-        </div>
-        <div v-if="loading" class="w-full flex flex-col items-center justify-center mt-4">
-            <loading-graphic inline text="Please wait while we diagnose your site." />
-            <p class="mt-1 text-sm text-gray">
-                This may take a few minutes.
-            </p>
-        </div>
-
-        <div v-if="success" class="w-full flex flex-col items-center justify-center mt-4">
-            <p class="mt-1 text-lg text-green-600">
-                Report created successfully! Redirecting...
-            </p>
-        </div>
+  <div class="max-w-xl">
+    <div class="card">
+      <div class="flex flex-row space-x-4">
+        <text-input class="w-full" placeholder="Domain" v-model="domain" />
+        <button class="btn-primary" :disabled="disabled" @click="createReport">
+          <div v-if="loading" class="flex items-center w-5 h-5 mr-2">
+            <loading-graphic inline text="" />
+          </div>
+          Create Report
+        </button>
+      </div>
+      <p v-if="error" v-text="error" class="my-2 text-sm text-red-500"></p>
     </div>
+    <div
+      v-if="loading"
+      class="flex flex-col items-center justify-center w-full mt-4"
+    >
+      <loading-graphic inline text="Please wait while we diagnose your site." />
+      <p class="mt-1 text-sm text-gray">This may take a few minutes.</p>
+    </div>
+
+    <div
+      v-if="success"
+      class="flex flex-col items-center justify-center w-full mt-4"
+    >
+      <p class="mt-1 text-lg text-green-600">
+        Report created successfully! Redirecting...
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['app_url', 'app_env', 'create_report_url'],
-        data() {
-            return {
-                domain: this.app_url,
-                error: null,
-                disabled: false,
-                loading: false,
-                success: false,
-                report: null,
-            }
-        },
-        mounted() {
-            if (this.app_env !== 'production') {
-                this.error = 'SEOPulse works only on a production environment';
-                this.disabled = true;
-            }
-        },
-        methods: {
-            createReport() {
-                if (this.domain.startsWith('http://')) {
-                    this.error = 'Please enter a domain with SSL';
-                    return ;
-                }
-
-                if(! this.domain.startsWith(this.app_url)) {
-                    this.error = 'Domain must start with ' + this.app_url;
-                    return ;
-                }
-
-                this.loading = true;
-                this.disabled = true;
-                this.error = null;
-
-                this.fetchReport();
-            },
-            fetchReport() {
-                Statamic.$request.post(this.create_report_url, { domain: this.domain })
-                    .then(({ data }) => {
-                        if (! data.success) {
-                            return;
-                        }
-
-                        this.loading = false;
-                        this.disabled = false;
-                        this.success = true;
-
-                        setTimeout(() => {
-                            window.location.href = cp_url(`seo-pulse/${data.data.id}`) ;
-                        }, 2000);
-                    });
-            }
-        }
+export default {
+  props: ['app_url', 'app_env', 'create_report_url'],
+  data() {
+    return {
+      domain: this.app_url,
+      error: null,
+      disabled: false,
+      loading: false,
+      success: false,
+      report: null,
+    };
+  },
+  mounted() {
+    if (this.app_env !== 'production') {
+      this.error = 'SEOPulse works only on a production environment';
+      this.disabled = true;
     }
+  },
+  methods: {
+    createReport() {
+      if (this.domain.startsWith('http://')) {
+        this.error = 'Please enter a domain with SSL';
+        return;
+      }
+
+      if (!this.domain.startsWith(this.app_url)) {
+        this.error = 'Domain must start with ' + this.app_url;
+        return;
+      }
+
+      this.loading = true;
+      this.disabled = true;
+      this.error = null;
+
+      this.fetchReport();
+    },
+    fetchReport() {
+      Statamic.$request
+        .post(this.create_report_url, { domain: this.domain })
+        .then(({ data }) => {
+          if (!data.success) {
+            return;
+          }
+
+          this.loading = false;
+          this.disabled = false;
+          this.success = true;
+
+          setTimeout(() => {
+            window.location.href = cp_url(`seopulse/${data.data.id}`);
+          }, 2000);
+        });
+    },
+  },
+};
 </script>
