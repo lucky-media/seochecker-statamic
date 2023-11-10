2615e739ff48453d6a4078d66248c3a7e1cda0fa
isni-sh <isnishabani.95@gmail.com>
2023-11-10 16:09:07 +0100

    add comments and fix conditions

--- resources/js/components/ui/ProgressBar.vue
+++ resources/js/components/ui/ProgressBar.vue
@@ -1,10 +1,13 @@
+<!-- 
+  If the result is greater than 100%, the indicator goes to the end of the progress bar; 
+  otherwise, 100% of the progress bar is in the orange part at the end.
+-->
+
 <template>
   <div>
-    <div class="flex sp-rounded-r" :class="[indicator === 100 && 'relative']">
-      <div
-        class="flex w-2/3 h-2 sp-rounded-l"
-        :class="[indicator < 100 && 'relative']"
-      >
+    {{ indicator }}
+    <div class="flex sp-rounded-r" :class="[indicator > 100 && 'relative']">
+      <div class="flex w-2/3 h-2" :class="[indicator <= 100 && 'relative']">
         <div class="flex-1 sp-bg-green-500 sp-rounded-l" />
         <div class="flex-1 sp-bg-yellow-500" />
         <!-- Indicator Position -->
@@ -12,6 +15,7 @@
           <div
             class="flex flex-col sp-items-center sp-justify-center sp-mt-1 sp-text-xs sp-transform -sp-translate-x-1/2"
           >
+            <!-- Triangle -->
             <div class="sp-inline-block sp-w-1.5 sp-overflow-hidden">
               <div
                 class="sp-bg-gray-700 sp-rotate-45 sp-transform sp-origin-bottom-left sp-h-1 sp-w-1"
@@ -34,8 +38,9 @@ export default {
   computed: {
     indicator: function () {
       let numberValue = this.value.split(' ')[0];
+      // Over max value
       if (numberValue > this.maxValue) {
-        return 100;
+        return 101;
       }
       return (numberValue / this.maxValue) * 100;
     },
