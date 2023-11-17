c515f6a2a35ee0f07442360b7f05e494b41f767d
isni-sh <isnishabani.95@gmail.com>
2023-11-17 11:15:14 +0100

    add pie chart to status legend

--- resources/js/components/ui/StatusLegend.vue
+++ resources/js/components/ui/StatusLegend.vue
@@ -1,22 +1,33 @@
 <template>
   <card class="max-w-xs">
     <card-header>Status Legend</card-header>
-    <card-body>
-      <div class="sp-space-y-3">
+    <card-body class="flex sp-items-center">
+      <div
+        class="grid sp-w-32 sp-h-32 sp-rounded-full sp-overflow-hidden sp-grid-cols-2 sp-relative"
+      >
+        <div
+          class="bg-white sp-w-24 sp-h-24 sp-absolute sp-rounded-full sp-left-1/2 sp-top-1/2 -sp-translate-x-1/2 -sp-translate-y-1/2"
+        ></div>
+        <div class="sp-bg-green-500"></div>
+        <div class="sp-bg-yellow-500"></div>
+        <div class="sp-bg-red-500"></div>
+        <div class="sp-bg-black"></div>
+      </div>
+      <div class="text-sm sp-space-y-2 sp-ml-5">
         <div class="flex sp-items-center">
-          <div class="w-5 h-5 mr-2 sp-rounded-full sp-bg-green-500" />
+          <div class="w-4 h-4 mr-2 sp-rounded-full sp-bg-green-500" />
           Passed
         </div>
         <div class="flex sp-items-center">
-          <div class="w-5 h-5 mr-2 sp-rounded-full sp-bg-yellow-500" />
+          <div class="w-4 h-4 mr-2 sp-rounded-full sp-bg-yellow-500" />
           Warning
         </div>
         <div class="flex sp-items-center">
-          <div class="w-5 h-5 mr-2 sp-rounded-full sp-bg-red-500" />
+          <div class="w-4 h-4 mr-2 sp-rounded-full sp-bg-red-500" />
           Failed
         </div>
         <div class="flex sp-items-center">
-          <div class="w-5 h-5 mr-2 sp-rounded-full sp-bg-black" />
+          <div class="w-4 h-4 mr-2 sp-rounded-full sp-bg-black" />
           Info
         </div>
       </div>
