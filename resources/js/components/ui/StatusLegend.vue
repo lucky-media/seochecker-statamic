a113b284a182acc8501c06743fbb22c57928255b
Lokman Musliu <lokman.musliu@gmail.com>
2023-11-17 09:22:03 -0600

    format

--- resources/js/components/ui/StatusLegend.vue
+++ resources/js/components/ui/StatusLegend.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="flex space-x-4 sp-justify-end">
-    <div v-for="item in legend" :key="item.value" class="flex sp-items-center">
-      <status :value="item.value" />
-      <span class="text-sm sp-pl-1">
+    <div class="flex space-x-4 sp-justify-end">
+        <div v-for="item in legend" :key="item.value" class="flex sp-items-center">
+            <status :value="item.value"/>
+            <span class="text-sm sp-pl-1">
           {{ item.label }}
       </span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Status from './Status.vue';
 
 export default {
-  components: {
-    Status,
-  },
+    components: {
+        Status,
+    },
     data() {
-      return {
-          legend: [
-              {
-                  value: 'passed',
-                  label: 'Passed',
-              },
-              {
-                  value: 'warning',
-                  label: 'Warning',
-              },
-              {
-                  value: 'failed',
-                  label: 'Failed',
-              },
-              {
-                  value: 'info',
-                  label: 'Info',
-              },
-          ]
-      }
+        return {
+            legend: [
+                {
+                    value: 'passed',
+                    label: 'Passed',
+                },
+                {
+                    value: 'warning',
+                    label: 'Warning',
+                },
+                {
+                    value: 'failed',
+                    label: 'Failed',
+                },
+                {
+                    value: 'info',
+                    label: 'Info',
+                },
+            ]
+        }
     }
 };
 </script>
