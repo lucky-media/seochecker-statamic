9d3d665639d7fc2c4a7692894efc62b625b64f94
isni-sh <isnishabani.95@gmail.com>
2023-11-20 17:46:13 +0100

    create component for info heading list

--- resources/js/pages/reports/sections/SeoContentSection.vue
+++ resources/js/pages/reports/sections/SeoContentSection.vue
@@ -1,39 +1,45 @@
 <template>
-    <card>
-        <card-header> SEO Content</card-header>
+  <card>
+    <card-header> SEO Content</card-header>
 
-        <info-item title="Heading Count" score="info">
-            <div class="sp-w-full sp-divide-gray-200 sp-divide-y">
-                <h1 class="py-2 leading-none">h1: {{ content.headingCount.h1 }}</h1>
-                <h2 class="py-2 leading-none">h2: {{ content.headingCount.h2 }}</h2>
-                <h3 class="py-2 leading-none">h3: {{ content.headingCount.h3 }}</h3>
-                <h4 class="py-2 leading-none">h4: {{ content.headingCount.h4 }}</h4>
-                <h5 class="py-2 leading-none">h5: {{ content.headingCount.h5 }}</h5>
-                <h5 class="py-2 leading-none">h6: {{ content.headingCount.h6 }}</h5>
-            </div>
-        </info-item>
+    <info-item title="Heading Count" score="info">
+      <div class="sp-w-full sp-divide-gray-200 sp-divide-y">
+        <info-list
+          title="h1"
+          :list="content.headingCount.h1"
+          :length="content.headingCount.h1.length"
+        />
 
+        <!-- <h2 class="py-2 leading-none">h2: {{ content.headingCount.h2 }}</h2>
+        <h3 class="py-2 leading-none">h3: {{ content.headingCount.h3 }}</h3>
+        <h4 class="py-2 leading-none">h4: {{ content.headingCount.h4 }}</h4>
+        <h5 class="py-2 leading-none">h5: {{ content.headingCount.h5 }}</h5>
+        <h5 class="py-2 leading-none">h6: {{ content.headingCount.h6 }}</h5> -->
+      </div>
+    </info-item>
 
-        <info-item
-            title="Multiple Headings"
-            :score="content.multiple_headings.score"
-        >
-            <p>{{ content.multiple_headings.description }}</p>
-        </info-item>
+    <info-item
+      title="Multiple Headings"
+      :score="content.multiple_headings.score"
+    >
+      <p>{{ content.multiple_headings.description }}</p>
+    </info-item>
 
-        <info-item title="Broken Images" :score="content.broken_images.score">
-            <p>{{ content.broken_images.description }}</p>
-        </info-item>
+    <info-item title="Broken Images" :score="content.broken_images.score">
+      <p>{{ content.broken_images.description }}</p>
+    </info-item>
 
+    <info-item title="Images without alt" :score="content.no_alt.score">
+      <p v-if="content.no_alt.value === 0">{{ content.no_alt.description }}</p>
+      <div
+        v-if="content.no_alt.value > 0"
+        class="sp-divide-gray-200 sp-divide-y"
+      >
+        <p v-if="item in content.no_alt.value">{{ item }}</p>
+      </div>
+    </info-item>
 
-        <info-item title="Images without alt" :score="content.no_alt.score">
-            <p v-if="content.no_alt.value === 0">{{ content.no_alt.description }}</p>
-            <div v-if="content.no_alt.value > 0" class="sp-divide-gray-200 sp-divide-y">
-                <p v-if="item in content.no_alt.value">{{ item }}</p>
-            </div>
-        </info-item>
-
-        <info-item
+    <!-- <info-item
             title="Links without describing text"
             :score="this.content.link_scores[0].score"
         >
@@ -49,26 +55,27 @@
             <p>
                 {{ this.content.link_scores[1].description }}
             </p>
-        </info-item>
-    </card>
+        </info-item> -->
+  </card>
 </template>
 
 <script>
 import CardHeader from '@/components/card/CardHeader.vue';
 import Card from '@/components/card/Card.vue';
 import InfoItem from '@/components/infolist/InfoItem.vue';
+import InfoList from '@/components/infolist/InfoList.vue';
 import unslugify from '@/services/unslugify';
 
 export default {
-    components: {
-        InfoItem,
-        Card,
-        CardHeader,
-    },
-
-    methods: {
-        unslugify,
-    },
-    props: ['content'],
+  props: ['content'],
+  components: {
+    InfoItem,
+    InfoList,
+    Card,
+    CardHeader,
+  },
+  methods: {
+    unslugify,
+  },
 };
 </script>
