f2082d2d422c0656989b2aeaa1624539828c9ebf
isni-sh <isnishabani.95@gmail.com>
2023-11-21 15:41:03 +0100

    refactor code

--- resources/js/components/infolist/HeadingList.vue
+++ resources/js/components/infolist/HeadingList.vue
@@ -4,10 +4,11 @@
       <component :is="tag" class="py-2 leading-none">
         {{ title }}: {{ length }}
       </component>
-      <button @click="showHide()" class="sp-text-xs sp-font-medium">
-        <template v-if="isHidden">Hide Headings</template>
-        <template v-if="!isHidden">Show Headings</template>
-      </button>
+      <button
+        @click="showHide()"
+        class="sp-text-xs sp-font-medium"
+        v-text="isHidden ? 'Hide' : 'Show'"
+      />
     </div>
     <ul v-if="isHidden" class="pb-2">
       <li v-for="value in list" :key="value">{{ value }}</li>
